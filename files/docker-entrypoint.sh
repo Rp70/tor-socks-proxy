3be99bbf40748870227cea4d25f37c9ed85ea5f1
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-08-02 09:38:05 +0700

    update

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -14,6 +14,7 @@ fi
 # Set defaults if noone specified.
 TOR_PROCESSES=${TOR_PROCESSES:=10}
 TOR_PORT_PREFIX=${TOR_PORT_PREFIX:=90}
-export TOR_PROCESSES TOR_PORT_PREFIX
+TOR_LISTEN_IP=${TOR_LISTEN_IP:='0.0.0.0'}
+export TOR_PROCESSES TOR_PORT_PREFIX TOR_LISTEN_IP
 
 exec supervisord --nodaemon;
