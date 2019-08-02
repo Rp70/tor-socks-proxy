2e813725d5b7399b9d3d3252aadcd539bcb753f2
Rp70 <14834373+Rp70@users.noreply.github.com>
2019-08-02 09:23:06 +0700

    update

--- files/docker-entrypoint.sh
+++ files/docker-entrypoint.sh
@@ -11,10 +11,12 @@ if [ "$CMD" != 'startup' ]; then
     exit $?
 fi
 
-
+# Set defaults if noone specified.
 TOR_PROCESSES=${TOR_PROCESSES:=10}
 TOR_PORT_PREFIX=${TOR_PORT_PREFIX:=90}
-export TOR_PROCESSES
+export TOR_PROCESSES TOR_PORT_PREFIX
+
+# Create data directory for each tor process.
 for i in $(seq 1 $TOR_PROCESSES); do
     mkdir -p /var/lib/tor/tor$i
 done
